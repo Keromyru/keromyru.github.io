6ea99becf958777d3f151a5d40ac9e539be625b4
Keromyru <aurora.dubnyk88@gmail.com>
2022-11-17 23:20:40 -0600

    Added SCSS to the project

--- aboutme-app/src/navigation/NavComponent.jsx
+++ aboutme-app/src/navigation/NavComponent.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export default function NavComponent() {
 	return(
 		<>
-      <h1>Aurora Dubnyk</h1>
+		<div className="nav-container">
+			<h1>Aurora Dubnyk</h1>
 			<p>Web App Developer</p>
 			<Link to="/resume">Resume</Link>
+		</div>
 		</>
 	)
 }
